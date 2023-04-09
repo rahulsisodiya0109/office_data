a3e732397c37ba578c447e3490da3ed66f41d616
Rahul <rahulsisodiya109@gmail.com>
2023-04-09 12:24:23 +0530

    waitlist_poc project committed.

---
+++ waitlist_poc_v1/src/test/java/com/waitlist/testCases/TC_SignupTest_003.java
@@ -0,0 +1,9 @@
+package com.waitlist.testCases;
+
+public class TC_SignupTest_003 extends BaseClass {
+
+//	to generate the random email for sign up can use randomInteger method from base class
+
+
+
+}
